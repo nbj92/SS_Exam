f8bc341ef7d9f27a22c9f0314fbeed39ffa9d50d
Nicolai Jensen <nbj_92@hotmail.com>
2024-05-23 12:39:16 +0200

    New files

---
+++ SS_Exam/Assets/Scripts/Alternativ/Animal_2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Alternativ
+{
+    [Serializable]
+    public class Animal_2
+    {
+
+        public Color Color { get; set; }
+        public float size;
+        public string AnimalName { get; set;}
+
+        public Animal_2(string name, float size, Color color)
+        {
+            this.AnimalName = name;
+            this.size = size;
+            this.Color = color;
+        }
+
+
+    }
+}
